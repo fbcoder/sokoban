f4a8287107cfa03e437fbb37eb08595732643c33
fbcoder <nep@fakeserver.fake>
2013-09-16 23:23:59 +0200

    Initial commit.

---
+++ leveldata.js
@@ -0,0 +1,177 @@
+var level = new Array();
+
+var levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[3] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[4] = "@@@@@@   @@@@@@@@@@@";
+levelDataLine[5] = "@@@@@@o  @@@@@@@@@@@";
+levelDataLine[6] = "@@@@@@  o@@@@@@@@@@@";
+levelDataLine[7] = "@@@@  o o @@@@@@@@@@";
+levelDataLine[8] = "@@@@ @ @@ @@@@@@@@@@";
+levelDataLine[9] = "@@   @ @@ @@@@@  xx@";
+levelDataLine[10] = "@@ o  o          xx@";
+levelDataLine[11] = "@@@@@@ @@@ @^@@  xx@";
+levelDataLine[12] = "@@@@@@     @@@@@@@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[0] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[3] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[4] = "@@@@xx  @     @@@@@@";
+levelDataLine[5] = "@@@@xx  @ o  o  @@@@";
+levelDataLine[6] = "@@@@xx  @o@@@@  @@@@";
+levelDataLine[7] = "@@@@xx    < @@  @@@@";
+levelDataLine[8] = "@@@@xx  @ @  o @@@@@";
+levelDataLine[9] = "@@@@@@@@@ @@o o @@@@";
+levelDataLine[10] = "@@@@@@ o  o o o @@@@";
+levelDataLine[11] = "@@@@@@    @     @@@@";
+levelDataLine[12] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[1] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[3] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[4] = "@@@@@@@@@@     <@@@@";
+levelDataLine[5] = "@@@@@@@@@@ o@o @@@@@";
+levelDataLine[6] = "@@@@@@@@@@ o  o@@@@@";
+levelDataLine[7] = "@@@@@@@@@@@o o @@@@@";
+levelDataLine[8] = "@@@@@@@@@@ o @ @@@@@";
+levelDataLine[9] = "@@xxxx  @@ o  o  @@@";
+levelDataLine[10] = "@@@xxx    o  o   @@@";
+levelDataLine[11] = "@@xxxx  @@@@@@@@@@@@";
+levelDataLine[12] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[2] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@  xxxx@";
+levelDataLine[3] = "@@@@@@@@@@@@@  xxxx@";
+levelDataLine[4] = "@@    @  o o   xxxx@";
+levelDataLine[5] = "@@ ooo@o  o @  xxxx@";
+levelDataLine[6] = "@@  o     o @  xxxx@";
+levelDataLine[7] = "@@ oo @o o o@@@@@@@@";
+levelDataLine[8] = "@@  o @     @@@@@@@@";
+levelDataLine[9] = "@@@ @@@@@@@@@@@@@@@@";
+levelDataLine[10] = "@@    @    @@@@@@@@@";
+levelDataLine[11] = "@@     o   @@@@@@@@@";
+levelDataLine[12] = "@@  oo@oo  <@@@@@@@@";
+levelDataLine[13] = "@@    @    @@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[3] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@   @@@@@@@";
+levelDataLine[3] = "@@@@@@@@@@ @o@@  @@@";
+levelDataLine[4] = "@@@@@@@@@@     o @@@";
+levelDataLine[5] = "@@@@@@@@@@ @@@   @@@";
+levelDataLine[6] = "@@xxxx  @@ o  o@@@@@";
+levelDataLine[7] = "@@xxxx    o oo @@@@@";
+levelDataLine[8] = "@@xxxx  @@o  o <@@@@";
+levelDataLine[9] = "@@@@@@@@@@  o  @@@@@";
+levelDataLine[10] = "@@@@@@@@@@ o o  @@@@";
+levelDataLine[11] = "@@@@@@@@@@@@ @@ @@@@";
+levelDataLine[12] = "@@@@@@@@@@@@    @@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[4] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[3] = "@@@@@xx  @@@@v@@@@@@";
+levelDataLine[4] = "@@@@@xx  @@@   @@@@@";
+levelDataLine[5] = "@@@@@xx     oo @@@@@";
+levelDataLine[6] = "@@@@@xx  @ @ o @@@@@";
+levelDataLine[7] = "@@@@@xx@@@ @ o @@@@@";
+levelDataLine[8] = "@@@@@@@@ o @o  @@@@@";
+levelDataLine[9] = "@@@@@@@@  o@ o @@@@@";
+levelDataLine[10] = "@@@@@@@@ o  o  @@@@@";
+levelDataLine[11] = "@@@@@@@@  @@   @@@@@";
+levelDataLine[12] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[5] = new levelObject(levelDataLine);
+
+levelDataLine = new Array();
+levelDataLine[0] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[1] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[2] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[3] = "@@@@@@@@@@@   @@@@@@";
+levelDataLine[4] = "@@@@@ @ <@@ oo @@@@@";
+levelDataLine[5] = "@@@@    o      @@@@@";
+levelDataLine[6] = "@@@@  o  @@@   @@@@@";
+levelDataLine[7] = "@@@@@@ @@@@@o@@@@@@@";
+levelDataLine[8] = "@@@@ o  @@@ xx@@@@@@";
+levelDataLine[9] = "@@@@ o o o xxx@@@@@@";
+levelDataLine[10] = "@@@@    @@@xxx@@@@@@";
+levelDataLine[11] = "@@@@ oo @@@xxx@@@@@@";
+levelDataLine[12] = "@@@@  @@@@@@@@@@@@@@";
+levelDataLine[13] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[14] = "@@@@@@@@@@@@@@@@@@@@";
+levelDataLine[15] = "@@@@@@@@@@@@@@@@@@@@";
+level[6] = new levelObject(levelDataLine);
+
+
+function levelObject(datalineArray){
+	this.cratesToPlace = 0;
+	this.levelMapOrig = new Array();
+	this.levelHeight = datalineArray.length;
+	for(i=0;i<this.levelHeight;i++){
+		this.levelMapOrig[i] = new Array();
+		this.levelWidth = datalineArray[i].length;
+		for(j=0;j<this.levelWidth;j++){
+			if(datalineArray[i].charAt(j) == "@"){
+				this.levelMapOrig[i][j] = 1;
+			}else if(datalineArray[i].charAt(j) == "o"){
+				this.levelMapOrig[i][j] = 2;
+			}else if(datalineArray[i].charAt(j) == "x"){
+				this.levelMapOrig[i][j] = 3;
+				this.cratesToPlace += 1;
+			}else if(datalineArray[i].charAt(j) == "^"){
+				this.levelMapOrig[i][j] = 4;
+				this.playerStartX = j;
+				this.playerStartY = i;
+				this.playerStartDir = 0;
+			}else if(datalineArray[i].charAt(j) == "v"){
+				this.levelMapOrig[i][j] = 4;
+				this.playerStartX = j;
+				this.playerStartY = i;
+				this.playerStartDir = 3;
+			}else if(datalineArray[i].charAt(j) == "<"){
+				this.levelMapOrig[i][j] = 4;
+				this.playerStartX = j;
+				this.playerStartY = i;
+				this.playerStartDir = 1;
+			}else if(datalineArray[i].charAt(j) == ">"){
+				this.levelMapOrig[i][j] = 4;
+				this.playerStartX = j;
+				this.playerStartY = i;
+				this.playerStartDir = 2;
+			}else {
+				this.levelMapOrig[i][j] = 0;
+			}	
+		}
+	}
+}
